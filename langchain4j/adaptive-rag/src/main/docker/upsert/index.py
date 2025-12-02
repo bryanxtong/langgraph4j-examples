489ffbd8b2a9ae34200f446c0df3b66d0c120977
bryanxtong <bryanxtong@gmail.com>
2025-12-02 12:35:09 +0800

    update to langchain 1.1.0

--- langchain4j/adaptive-rag/src/main/docker/upsert/index.py
+++ langchain4j/adaptive-rag/src/main/docker/upsert/index.py
@@ -3,9 +3,9 @@
 import chromadb
 from chromadb.config import Settings
 
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.document_loaders import WebBaseLoader
-from langchain_community.vectorstores import Chroma
+from langchain_chroma import Chroma
 from langchain_openai import OpenAIEmbeddings
 
 ## see https://stackoverflow.com/a/77925278/521197
